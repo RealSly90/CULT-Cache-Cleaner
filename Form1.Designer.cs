1c192c29db263e4038c139563250644c69166778
fabio <fabio@LEGION-FABIO>
2024-02-28 15:11:16 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace CULT_Cache_Cleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(45, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(337, 80);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Pulizia Standard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(413, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(337, 80);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pulizia Completa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.LightCoral;
+            this.label1.Location = new System.Drawing.Point(236, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seleziona il metodo di cancellazione preferito:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(277, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(440, 46);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CultRP Cache Cleaner 1.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(281, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(437, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Questo software è stato creato da RealSly e TheMorgan per ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(370, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(281, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(469, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "facilitare la pulizia della cache di FiveM, ed è fornito gratuitamente";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(281, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(299, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "a tutti gli utenti della community di CultRP";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label7.Location = new System.Drawing.Point(82, 349);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(261, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Consigliata per una pulizia periodica";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label8.Location = new System.Drawing.Point(450, 349);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(267, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Consigliata per grandi aggiornamenti";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label9.Location = new System.Drawing.Point(480, 369);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(197, 20);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "o se stai cambiando server";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CULT_Cache_Cleaner.Properties.Resources.FiveM_Logo;
+            this.pictureBox2.Location = new System.Drawing.Point(745, 407);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(43, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CULT_Cache_Cleaner.Properties.Resources.blank;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label10.Location = new System.Drawing.Point(439, 389);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(279, 20);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "verranno resettate le associazioni tasti";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CultRP Cache Cleaner for FiveM";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CULT_Cache_Cleaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Comandi per eliminare le cartelle
+                string[] commands = {
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\cache""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\nui-storage""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\nui-storage-fxdk""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\server-cache""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\server-cache-fxdk""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\server-cache-priv"""
+                };
+
+                // Esecuzione dei comandi
+                foreach (string command in commands)
+                {
+                    ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+                    processInfo.CreateNoWindow = true;
+                    processInfo.UseShellExecute = false;
+                    Process.Start(processInfo).WaitForExit();
+                }
+
+                MessageBox.Show("Cartelle eliminate con successo, puoi chiudere l'app e avviare FiveM.", "Operazione completata", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Si è verificato un errore durante l'eliminazione delle cartelle:\n\n" + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Comandi per eliminare le cartelle e il file
+                string[] commands = {
+                    @"del /q ""%appdata%\CitizenFX\fivem.cfg""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\cache""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\nui-storage""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\nui-storage-fxdk""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\server-cache""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\server-cache-fxdk""",
+                    @"rmdir /s /q ""%LocalAppData%\FiveM\FiveM.app\data\server-cache-priv"""
+                };
+
+                // Esecuzione dei comandi
+                foreach (string command in commands)
+                {
+                    ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+                    processInfo.CreateNoWindow = true;
+                    processInfo.UseShellExecute = false;
+                    Process.Start(processInfo).WaitForExit();
+                }
+
+                MessageBox.Show("File e cartelle eliminate con successo, chiudi l'app e avvia FiveM.", "Operazione completata", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Si è verificato un errore durante l'eliminazione dei file e delle cartelle:\n\n" + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
